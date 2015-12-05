eaa9d13d24d377aabb3ad1fd94f5257ff3714bb1
Rohit Bansal <rohit@bansalrohit.com>
2015-12-05 14:53:58 -0800

    Added placeholder IMUSubsystemTest class

---
+++ CompetitionTemplate/tests/competition/subsystems/sensor/IMUSubsystemTest.java
@@ -0,0 +1,17 @@
+package competition.subsystems.sensor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import xbot.common.injection.BaseWPITest;
+
+public class IMUSubsystemTest extends BaseWPITest {
+    
+    @Test
+    public void testTankDrive() {
+        //TODO: ValidateIMUSubsystem validateIMUSubsystem = this.injector.getInstance(ValidateIMUSubsystem.class);
+        
+        //validateIMUSubsystem.getCurrentCommand();
+    }
+}
